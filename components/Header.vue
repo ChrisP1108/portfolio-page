eee760cd9d2d137f47547abc4c92a36030e7cd64
Chris Paschall <ChrisP1108@gmail.com>
2021-11-26 18:15:47 -0700

    Initial Test

---
+++ components/Header.vue
@@ -0,0 +1,12 @@
+<!-- Please remove this file from your project -->
+<template>
+  <h1>Hello World</h1>
+</template>
+
+<style lang="scss">
+  body {
+    h1 {
+      color: blue;
+    }
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="main">
-    <Tutorial/>
+    <Header/>
   </div>
 </template>
 
