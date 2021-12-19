262b59fc27986fa431bb31dd14ca7bb47b7816a5
Chris Paschall <ChrisP1108@gmail.com>
2021-12-19 16:13:39 -0700

    Completion

--- components/Loading.vue
+++ components/Loading.vue
@@ -2,7 +2,7 @@
     <div class="loading-container">
         <img src="../assets/images/logo.svg"  class="hover-glow" 
         alt="logo">
-        <h1 class="orange-text">Loading</h1>
+        <h1 class="orange-text fade-in">Loading</h1>
     </div>
 </template>
 
@@ -21,8 +21,20 @@
     h1 {
         font-size: 15vw;
         margin-top: 1vw;
+        animation-name: loadingFadeIn;
+        animation-duration: 2s;
+        animation-fill-mode: forwards;
     }
     img {
         width: 30vw;
+        animation-name: glow;
+        animation-duration: 2s;
+        animation-timing-function: linear;
+        animation-iteration-count: infinite;
+    }
+    .fade-in {
+        animation-name: fadeIn;
+        animation-duration: 1s;
+        animation-fill-mode: forwards;
     }
 </style>
